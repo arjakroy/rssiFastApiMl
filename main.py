fc1c2dca3530628a9f3483a6515ebcaf78419184
arjakroy <149053070+arjakroy@users.noreply.github.com>
2024-09-05 10:43:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,19 +7,13 @@ import pandas as pd
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods = ["*"],
-    allow_headers = ["*"]
+  allow_origins = ["*"],
+  allow_methods = ["*"],
+  allow_headers = ["*"]
+
 
 )
 
