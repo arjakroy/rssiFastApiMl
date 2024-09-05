54ded86bedee0984b4a90bfa22d4dfb5dd58278b
arjakroy <149053070+arjakroy@users.noreply.github.com>
2024-09-05 10:16:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,23 @@ from rssi import Rssi
 import numpy as np
 import pickle
 import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+
+)
+
 pickle_in = open("model.pkl", "rb")
 model = pickle.load(pickle_in)
 @app.get("/")
